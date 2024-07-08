8c258bb173a467050f1d152c80693d4bcc72c4f1
Miras <101412464+TauAdam@users.noreply.github.com>
2024-07-08 23:03:45 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,6 @@ const token = process.env.BOT_TOKEN
 
 const bot = new TelegramBot(token, { polling: true })
 
-bot.onText(/\/echo (.+)/, (msg, match) => {
-
-	const chatId = msg.chat.id
-	const resp = match[1]
-
-	bot.sendMessage(chatId, resp)
-})
 bot.on('message', async (msg) => {
 	const chatId = msg.chat.id
 
